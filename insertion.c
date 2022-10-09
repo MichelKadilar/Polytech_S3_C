4f74e398e3e51ca4e6bec267bcb1b9defbe8ea66
Michel Kadilar <michel.kadilar@ensiie.fr>
2022-10-09 20:10:25 +0200

    insertion.c

--- insertion.c
+++ insertion.c
@@ -4,19 +4,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void insertionDansTableau(int **tableau, int tailleTableau,
-                          int valeurAInserer) {
-    int *tableau2 = (int *) calloc((tailleTableau + 1), sizeof(int));
-    for (int i = 0; i < tailleTableau; i++) {
-        tableau2[i] = tableau[i];
-    }
-    tableau = (int *) calloc((tailleTableau + 1), sizeof(int));
-    tableau[tailleTableau] = valeurAInserer;
-    for (int i = tailleTableau; i > 0; i--) {
-        if (valeurAInserer < tableau2[i - 1]) {
-            tableau[i] = tableau2[i - 1];
-            tableau2[i - 1] = valeurAInserer;
+void insertionDansTableau(int **tableau, int nbElementsDansTableau, int valeurAInserer) {
+    *tableau = realloc(*tableau, (nbElementsDansTableau + 1) * sizeof(int));
+    (*tableau)[nbElementsDansTableau] = valeurAInserer;
+    for (int i = nbElementsDansTableau; i > 0; i--) {
+        int j = i;
+        while ((*tableau)[j] < (*tableau)[j - 1]) {
+            int temp = (*tableau)[j];
+            (*tableau)[j] = (*tableau)[j - 1];
+            (*tableau)[j - 1] = temp;
+            j--;
         }
     }
 }
@@ -24,8 +23,13 @@ void insertionDansTableau(int **tableau, int tailleTableau,
 int main(int argc, char *argv[]) {
     int tailleTableauEntree =
             argc - 1; // -1 car on veut enlever le nom de l'executable des paramètres pour notre décompte.
-    int tableau[tailleTableauEntree];
+    int compteurNbElementsDansTableau = 0;
+    int *tableau = malloc(0);
     for (int i = 0; i < tailleTableauEntree; i++) {
-        tableau[i] = atoi(argv[i + 1]);
+        insertionDansTableau(&tableau, compteurNbElementsDansTableau, atoi(argv[i + 1]));
+        compteurNbElementsDansTableau++;
+    }
+    for (int indiceAffichage = 0; indiceAffichage < tailleTableauEntree; indiceAffichage++) {
+        printf("%d ", tableau[indiceAffichage]);
     }
 }
